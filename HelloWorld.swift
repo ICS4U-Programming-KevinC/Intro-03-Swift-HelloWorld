f684736feb492bf023e64bc821015af1063c3568
Kevin Csiffary <kevin.csiffary@stu.ocsb.ca>
2024-02-08 03:05:20 +0000

    Created HelloWorld.swift dialogue program

---
+++ HelloWorld.swift
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Kevin Csiffary
+//  Created on 2024-02-07
+//  Version 1.0
+//  Copyright (c) 2024 Kevin Csiffary. All rights reserved.
+//
+//  Shows a simple dialogue to the console.
+
+// Display the dialogue to the console
+print("Hey Ioana!")
+print("How's it going!")
